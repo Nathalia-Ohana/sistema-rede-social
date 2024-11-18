7dd37c8326d4c29e7551292622acc1fe1a491c40
NATHÁLIA OHANA CORREA DOS SANTOS <nathalia_o_santos@estudante.sesisenai.org.br>
2024-11-18 18:28:26 -0300

    feat: criação das classes Usuario, Comentario e Post e implementação dos métodos adicionarAmigo, removerAmigo e adicionarPost

---
+++ RedeSocial/src/modelo/Usuario.java
@@ -0,0 +1,132 @@
+package modelo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+public class Usuario {
+    private Integer id;
+
+    private String nome;
+
+    private String username;
+
+    private String email;
+
+    private String senha;
+
+    private LocalDateTime dataCadastro;
+
+    private List<Usuario> amigos;
+
+    private List<Post> posts;
+
+    public Usuario(String nome, String username, String email, String senha, LocalDateTime dataCadastro, List<Usuario> amigos, List<Post> posts) {
+        this.nome = nome;
+        this.username = username;
+        this.email = email;
+        this.senha = senha;
+        this.dataCadastro = LocalDateTime.now();
+        this.amigos = amigos;
+        this.posts = posts;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public List<Usuario> getAmigos() {
+        return amigos;
+    }
+
+    public void setAmigos(List<Usuario> amigos) {
+        this.amigos = amigos;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", dataCadastro=" + dataCadastro +
+                ", amigos=" + amigos +
+                ", posts=" + posts +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario usuario)) return false;
+        return getId().equals(usuario.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+    public void adicionarAmigo(Usuario amigo){
+    }
+
+    public void removerAmigo(Usuario amigo){
+    }
+
+    public void adicionarPost(Post post){
+    }
+}
+
