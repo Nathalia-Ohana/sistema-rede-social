fd2a50f032c68bbeb57293337e6680bc31b2036b
Nathália Ohana <nathalia.ohana2468@gmail.com>
2024-11-23 16:45:47 -0300

    feat: criação da classe PostException

---
+++ RedeSocial/src/com/redesocial/exception/PostException.java
@@ -0,0 +1,12 @@
+package com.redesocial.exception;
+
+public class PostException extends RuntimeException {
+
+    public PostException(String message) {
+        super(message);
+    }
+
+    public PostException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
