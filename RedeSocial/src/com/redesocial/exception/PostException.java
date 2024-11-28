d6a9bb4e3702d85aa561c5ea145a368c8752f096
Nathália Ohana <nathalia.ohana2468@gmail.com>
2024-11-28 20:45:09 -0300

    docs: documentação da classe PostException

--- RedeSocial/src/com/redesocial/exception/PostException.java
+++ RedeSocial/src/com/redesocial/exception/PostException.java
@@ -1,10 +1,28 @@
 package com.redesocial.exception;
 
+/**
+ * Exceção personalizada para erros relacionados a operações com posts na rede social.
+ * <p>
+ * Esta classe estende {@link RuntimeException} e é utilizada para representar problemas
+ * que possam ocorrer durante a criação, manipulação ou outras operações envolvendo posts.
+ * </p>
+ */
 public class PostException extends RuntimeException {
-
+    /**
+     * Construtor que cria uma exceção de post com uma mensagem específica.
+     *
+     * @param message Mensagem descritiva do erro relacionado ao post.
+     */
     public PostException(String message) {
         super(message);
     }
+
+    /**
+     * Construtor que cria uma exceção de post com uma mensagem específica e a causa subjacente.
+     *
+     * @param message Mensagem descritiva do erro relacionado ao post.
+     * @param cause   A causa subjacente do erro (outra exceção que originou este erro).
+     */
 
     public PostException(String message, Throwable cause) {
         super(message, cause);
