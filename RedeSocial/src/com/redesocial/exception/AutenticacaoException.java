11cf5f0a785c571a85d50f619268a02966a2831d
Nathália Ohana <nathalia.ohana2468@gmail.com>
2024-11-28 20:43:30 -0300

    docs: documentação da classe AutenticacaoException

--- RedeSocial/src/com/redesocial/exception/AutenticacaoException.java
+++ RedeSocial/src/com/redesocial/exception/AutenticacaoException.java
@@ -1,9 +1,28 @@
 package com.redesocial.exception;
 
-public class AutenticacaoException extends RuntimeException{
+/**
+ * Exceção personalizada para erros relacionados à autenticação na rede social.
+ * <p>
+ * Esta classe estende {@link RuntimeException} e é usada para sinalizar problemas
+ * que ocorrem durante os processos de autenticação.
+ * </p>
+ */
+public class AutenticacaoException extends RuntimeException {
+    /**
+     * Construtor que cria uma exceção de autenticação com uma mensagem específica.
+     *
+     * @param message Mensagem descritiva do erro de autenticação.
+     */
     public AutenticacaoException(String message) {
         super(message);
     }
+
+    /**
+     * Construtor que cria uma exceção de autenticação com uma mensagem específica e a causa subjacente.
+     *
+     * @param message Mensagem descritiva do erro de autenticação.
+     * @param cause   A causa subjacente do erro (outra exceção).
+     */
 
     public AutenticacaoException(String message, Throwable cause) {
         super(message, cause);
